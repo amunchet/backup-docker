5f0c05d2dbfd9aa7c92551bc7e46923cbc42c11b
Chester Enright <amunchet@gmail.com>
2024-05-21 16:28:31 -0500

    feat: working on productionizing

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ This project is a docker (or a standalone installation) and checks a specified f
 - Logs errors to syslog (or rsyslog)
 - Intended to run as a CRON
 
+## Dropbox and OAuth2
+1.  Run `obtain_refresh_token.py`.  This should result in a URL
+2.  Open that URL from a browser.  Click through it and it should redirect you to a URL that end with `?code=XXXX`
+3.  Copy that `XXXX` value
+4.  Run `obtain_refresh_token2.py`.  Paste the `XXXX` value there
+5.  Copy the Refresh token and paste it into `.env`
--- backup-docker/backup.py
+++ backup-docker/backup.py
@@ -1,99 +1,140 @@
-import sys
 import os
+import time
+import hashlib
+import sys
 import dropbox
-from dropbox.files import WriteMode
+import requests
+import pid
 from dropbox.exceptions import ApiError, AuthError
-
-# Add OAuth2 access token here.
-# You can generate one for yourself in the App Console.
-# See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
-
+from dropbox.files import WriteMode
 from dotenv import load_dotenv
+from backup_logger import logger
+
+
+# Load environment variables from .env file
 load_dotenv()
 
-TOKEN = os.environ.get("DROPBOX_TOKEN")
+# Environment variables
+WATCH_FOLDER = os.getenv("WATCH_FOLDER")
+DROPBOX_FOLDER = os.getenv("DROPBOX_FOLDER")
+DROPBOX_TOKEN = os.getenv("DROPBOX_TOKEN")
+DROPBOX_REFRESH_TOKEN = os.getenv("DROPBOX_REFRESH_TOKEN")
+APP_KEY = os.getenv("APP_KEY")
+APP_SECRET = os.getenv("APP_SECRET")
 
-LOCALFILE = 'my-file.txt'
-BACKUPPATH = '/test/my-file-backup.txt'
+# Function to refresh the Dropbox access token
+def refresh_access_token():
+    token_url = "https://api.dropbox.com/oauth2/token"
+    logger.debug(DROPBOX_REFRESH_TOKEN)
+    data = {
+        "grant_type": "refresh_token",
+        "refresh_token": DROPBOX_REFRESH_TOKEN,
+        "client_id": APP_KEY,
+        "client_secret": APP_SECRET,
+    }
 
-# Uploads contents of LOCALFILE to Dropbox
-def backup():
-    with open(LOCALFILE, 'rb') as f:
-        # We use WriteMode=overwrite to make sure that the settings in the file
-        # are changed on upload
-        print("Uploading " + LOCALFILE + " to Dropbox as " + BACKUPPATH + "...")
-        try:
-            dbx.files_upload(f.read(), BACKUPPATH, mode=WriteMode('overwrite'))
-        except ApiError as err:
-            # This checks for the specific error where a user doesn't have
-            # enough Dropbox space quota to upload this file
-            if (err.error.is_path() and
-                    err.error.get_path().reason.is_insufficient_space()):
-                sys.exit("ERROR: Cannot back up; insufficient space.")
-            elif err.user_message_text:
-                print(err.user_message_text)
-                sys.exit()
-            else:
-                print(err)
-                sys.exit()
+    response = requests.post(token_url, data=data)
+    if response.status_code != 200:
+        logger.error(response.text)
+        sys.exit("ERROR: Unable to refresh access token.")
 
-# Change the text string in LOCALFILE to be new_content
-# @param new_content is a string
-def change_local_file(new_content):
-    print("Changing contents of " + LOCALFILE + " on local machine...")
-    with open(LOCALFILE, 'wb') as f:
-        f.write(new_content)
+    tokens = response.json()
+    new_access_token = tokens['access_token']
 
-# Restore the local and Dropbox files to a certain revision
-def restore(rev=None):
-    # Restore the file on Dropbox to a certain revision
-    print("Restoring " + BACKUPPATH + " to revision " + rev + " on Dropbox...")
-    dbx.files_restore(BACKUPPATH, rev)
+    # Update the environment variable and .env file
+    os.environ["DROPBOX_TOKEN"] = new_access_token
+    with open(".env", "a") as env_file:
+        env_file.write(f"DROPBOX_TOKEN={new_access_token}\n")
 
-    # Download the specific revision of the file at BACKUPPATH to LOCALFILE
-    print("Downloading current " + BACKUPPATH + " from Dropbox, overwriting " + LOCALFILE + "...")
-    dbx.files_download_to_file(LOCALFILE, BACKUPPATH, rev)
+    return new_access_token
 
-# Look at all of the available revisions on Dropbox, and return the oldest one
-def select_revision():
-    # Get the revisions for a file (and sort by the datetime object, "server_modified")
-    print("Finding available revisions on Dropbox...")
-    entries = dbx.files_list_revisions(BACKUPPATH, limit=30).entries
-    revisions = sorted(entries, key=lambda entry: entry.server_modified)
+# Dropbox client abstraction
+class DropboxClient:
+    def __init__(self, dbx):
+        self.dbx = dbx
 
-    for revision in revisions:
-        print(revision.rev, revision.server_modified)
+    def upload(self, file_path, dropbox_path):
+        with open(file_path, 'rb') as f:
+            logger.info(f"Uploading {file_path} to Dropbox as {dropbox_path}...")
+            try:
+                self.dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+            except ApiError as err:
+                if err.error.is_path() and err.error.get_path().reason.is_insufficient_space():
+                    sys.exit("ERROR: Cannot back up; insufficient space.")
+                elif err.user_message_text:
+                    logger.error(err.user_message_text)
+                    sys.exit()
+                else:
+                    logger.error(err)
+                    sys.exit()
 
-    # Return the oldest revision (first entry, because revisions was sorted oldest:newest)
-    return revisions[0].rev
+# Function to calculate the MD5 checksum of a file
+def calculate_md5(file_path):
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
-if __name__ == '__main__':
-    # Check for an access token
-    if (len(TOKEN) == 0):
-        sys.exit("ERROR: Looks like you didn't add your access token. "
-            "Open up backup-and-restore-example.py in a text editor and "
-            "paste in your token in line 14.")
+# Function to read checksums from a file
+def read_checksums(file_path):
+    checksums = {}
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            for line in f:
+                file_name, file_md5 = line.strip().split()
+                checksums[file_name] = file_md5
+    return checksums
 
-    # Create an instance of a Dropbox class, which can make requests to the API.
-    print("Creating a Dropbox object...")
-    with dropbox.Dropbox(TOKEN) as dbx:
+# Function to write checksums to a file
+def write_checksums(file_path, checksums):
+    with open(file_path, "w") as f:
+        for file_name, file_md5 in checksums.items():
+            f.write(f"{file_name} {file_md5}\n")
 
-        # Check that the access token is valid
-        try:
-            dbx.users_get_current_account()
-        except AuthError:
-            sys.exit("ERROR: Invalid access token; try re-generating an "
-                "access token from the app console on the web.")
+# Main function to monitor the folder and upload changed files
+def monitor_and_upload(dropbox_client):
+    checksums_file = os.path.join(WATCH_FOLDER, "checksums.txt")
 
-        # Create a backup of the current settings file
-        backup()
+    # Read existing checksums
+    old_checksums = read_checksums(checksums_file)
+    new_checksums = {}
 
-        # Change the user's file, create another backup
-        change_local_file(b"updated")
-        backup()
+    # Calculate new checksums and compare with old ones
+    for file_name in os.listdir(WATCH_FOLDER):
+        file_path = os.path.join(WATCH_FOLDER, file_name)
+        if os.path.isfile(file_path):
+            file_md5 = calculate_md5(file_path)
+            new_checksums[file_name] = file_md5
+            if old_checksums.get(file_name) != file_md5:
+                dropbox_path = os.path.join(DROPBOX_FOLDER, file_name)
+                dropbox_client.upload(file_path, dropbox_path)
+                logger.info(f"Uploaded {file_name} to Dropbox.")
 
-        # Restore the local and Dropbox files to a certain revision
-        to_rev = select_revision()
-        restore(to_rev)
+    # Write new checksums to file
+    write_checksums(checksums_file, new_checksums)
 
-        print("Done!")
+def main():
+    if not WATCH_FOLDER or not DROPBOX_FOLDER or not DROPBOX_TOKEN or not DROPBOX_REFRESH_TOKEN or not APP_KEY or not APP_SECRET:
+        logger.error("Please set WATCH_FOLDER, DROPBOX_FOLDER, DROPBOX_TOKEN, DROPBOX_REFRESH_TOKEN, APP_KEY, and APP_SECRET environment variables.")
+        sys.exit(1)
+
+    try:
+        dbx = dropbox.Dropbox(DROPBOX_TOKEN)
+        dbx.users_get_current_account()
+    except AuthError:
+        # Refresh the access token if it's expired
+        new_token = refresh_access_token()
+        dbx = dropbox.Dropbox(new_token)
+
+    dropbox_client = DropboxClient(dbx)
+    monitor_and_upload(dropbox_client)
+    logger.info("Done!")
+
+if __name__ == "__main__":
+    logger.info("Starting up...")
+    while True:
+        with pid.PidFile():
+            main()
+            logger.info("Sleeping...")
+            time.sleep(60)
